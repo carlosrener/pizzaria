45bab3d2c3fc94d4b382da4756dbd127f340ffde
carlosrener <111909053+carlosrener@users.noreply.github.com>
2023-01-10 22:45:20 -0300

    finalizado web

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -5,6 +5,9 @@ import { Header } from "../../components/ui/Header";
 import styles from "./styles.module.scss";
 import { FiRefreshCcw } from "react-icons/fi";
 import { setupAPIClient } from "../../services/api";
+import Modal from "react-modal";
+import { ModalOrder } from "../../components/ui/ModalOrder";
+
 
 type OrderProps = {
   id: string;
@@ -13,16 +16,74 @@ type OrderProps = {
   draft: boolean;
   name: string | null;
 };
+
 interface HomeProps {
   orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  };
+  order: {
+    id: string;
+    table: string | number;
+    status: boolean;
+    name: string | null;
+  };
+};
+
 export default function Dashboard({ orders }: HomeProps) {
   const [orderList, setOrderList] = useState(orders || []);
 
-  function handleOpenModalView(id: string){
-    alert('Id clicado ' + id)
+  //use states para ao modal
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function handleCloseModal(){
+    setModalVisible(false)
   }
+
+  async function handleOpenModalView(id: string) {
+    const apiClient = setupAPIClient();
+    const response = await apiClient.get("order/detail", {
+      params: {
+        order_id: id,
+      },
+    });
+    setModalItem(response.data);
+    setModalVisible(true);
+  }
+
+  async function handleFinishItem(id: string){
+    const apiClient = setupAPIClient();
+
+    await apiClient.put('/order/finish',{
+      order_id: id,
+    })
+
+    const response = await apiClient.get('/order')
+    setOrderList(response.data)
+
+    setModalVisible(false)
+  }
+
+  async function handleRefreshOrders() {
+    const apiclient = setupAPIClient();
+    const response = await apiclient.get('/order');
+    setOrderList(response.data)
+  }
+
+  //modal
+  Modal.setAppElement("#__next");
 
   return (
     <>
@@ -35,12 +96,17 @@ export default function Dashboard({ orders }: HomeProps) {
         <main className={styles.container}>
           <div className={styles.containerHeader}>
             <h1>Ùltimos Pedidos</h1>
-            <button>
+            <button onClick={handleRefreshOrders}>
               <FiRefreshCcw size={25} color="#3fffa3" />
             </button>
           </div>
 
           <article className={styles.listOrders}>
+
+            {orderList.length === 0 && (
+              <span className={styles.emptyList}>Voce não tem pedido aberto...</span>
+            )}
+
             {orderList.map((item) => (
               <section key={item.id} className={styles.orderItem}>
                 <button onClick={() => handleOpenModalView(item.id)}>
@@ -51,10 +117,20 @@ export default function Dashboard({ orders }: HomeProps) {
             ))}
           </article>
         </main>
+        {/* modal */}
+        {modalVisible && (
+          <ModalOrder
+          isOpen={modalVisible}
+          onRequestClose={handleCloseModal}
+          order={modalItem}
+          handleFinishOrder={ handleFinishItem}/>
+        )}
       </div>
     </>
   );
 }
+
+
 
 //estrutura de server side
 export const getServerSideProps = canSSRAuth(async (ctx) => {
@@ -68,3 +144,5 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
     },
   };
 });
+
+//parei no inicio do video 117
